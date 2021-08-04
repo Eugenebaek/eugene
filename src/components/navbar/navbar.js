c672f457091ff4db2d449602dbf66f8de77ae2df
eugenebaek <eugenebaek5100@gmail.com>
2021-08-03 18:44:25 -0700

    created navbar

---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar'
+import {Typography} from "@material-ui/core";
+import useStyles from './styles';
+
+function Navbar() {
+    const classes = useStyles;
+    return (
+        <AppBar className={classes.appBar}>
+            <Typography>Hello World</Typography>
+        </AppBar>
+    );
+}
+
+export default Navbar;
---
+++ src/components/navbar/styles.js
@@ -0,0 +1,22 @@
+import {makeStyles} from "@material-ui/core";
+
+export default makeStyles({
+    root: {
+        background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+        border: 0,
+        borderRadius: 3,
+        boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+        color: 'white',
+        height: 48,
+        padding: '0 30px',
+    },
+    appBar: {
+        borderRadius: 15,
+        margin: '30px 0',
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        padding: '10px 50px',
+    },
+});
